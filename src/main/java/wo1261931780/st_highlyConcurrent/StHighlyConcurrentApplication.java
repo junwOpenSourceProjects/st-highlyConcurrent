b743e40f262aecc7a6820f224d5ba3e1bd825f67
junw <wo1261931780@gmail.com>
2024-06-06 15:44:19 +0800

    完成测试

---
+++ src/main/java/wo1261931780/st_highlyConcurrent/StHighlyConcurrentApplication.java
@@ -0,0 +1,13 @@
+package wo1261931780.st_highlyConcurrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StHighlyConcurrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StHighlyConcurrentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wo1261931780/st_highlyConcurrent/StHighlyConcurrentApplicationTests.java
@@ -0,0 +1,13 @@
+package wo1261931780.st_highlyConcurrent;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+//
+// @SpringBootTest
+// class StHighlyConcurrentApplicationTests {
+//
+// 	@Test
+// 	void contextLoads() {
+// 	}
+//
+// }
